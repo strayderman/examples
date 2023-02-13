6b28ff609c9409678d79e67abfa24f2dfefc49cc
Vitali Basko <strayderman@mail.ru>
2023-02-13 10:29:58 +0300

    example010 MetodArray

--- example009_IntroArray/Program.cs
+++ example009_IntroArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//               0   1   2   3   4   5   6   7   8
+int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
