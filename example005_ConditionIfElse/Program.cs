2cf80c996186f43334121214939aab8c7cf684b0
Vitali Basko <strayderman@mail.ru>
2023-02-19 22:03:01 +0300

    add example006

--- example005_ConditionIfElse/Program.cs
+++ example005_ConditionIfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите имя пользователя ");
 string username = Console.ReadLine()!;
 
-if (username.ToLower() == "маша")
+if(username.ToLower() == "маша")  // .ToLower() - приводит буквы к нижнему регистру
 {
     Console.WriteLine("Ура, это же МАША!");
 }
