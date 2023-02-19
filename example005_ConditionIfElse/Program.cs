58a31fadf1c8e4ed620814fac2652c2e1c86517f
Vitali Basko <strayderman@mail.ru>
2023-02-19 11:27:26 +0300

    change 003 and 005

--- example005_ConditionIfElse/Program.cs
+++ example005_ConditionIfElse/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("Введите имя пользователя ");
-string username = Console.ReadLine();
+string username = Console.ReadLine()!;
 
-if(username.ToLower() == "маша" )
+if (username.ToLower() == "маша")
 {
-    Console.WriteLine("Ура, это же МАША!" );
+    Console.WriteLine("Ура, это же МАША!");
 }
 else
 {
-    Console.Write("Привет, " );
+    Console.Write("Привет, ");
     Console.WriteLine(username);
 }
